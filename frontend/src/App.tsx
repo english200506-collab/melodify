b4661eca36174504d88907c72c2da0858ee86ab2
Asanbekov Beksultan <bek20052403@gmail.com>
2025-11-12 18:35:29 +0600

    Add music player (play, pause, volume, rewind)

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,21 +1,36 @@
-import {SignedIn, SignedOut, SignInButton, UserButton} from "@clerk/clerk-react";
-import {Button} from "@/components/ui/button.tsx";
+import { Route, Routes } from "react-router-dom";
+import HomePage from "./pages/home/HomePage";
+import AuthCallbackPage from "./pages/auth-callback/AuthCallbackPage";
+import { AuthenticateWithRedirectCallback } from "@clerk/clerk-react";
+import MainLayout from "./layout/MainLayout";
+import ChatPage from "./pages/chat/ChatPage";
+import AlbumPage from "./pages/album/AlbumPage";
+import AdminPage from "./pages/admin/AdminPage";
 
-const App = () => {
+import { Toaster } from "react-hot-toast";
+import NotFoundPage from "./pages/404/NotFoundPage";
+
+function App() {
     return (
-        <div>
-            <header>
-                <SignedOut>
-                    <SignInButton>
-                        <Button>Sign in</Button>
-                    </SignInButton>
-                </SignedOut>
-                <SignedIn>
-                    <UserButton />
-                </SignedIn>
-            </header>
-        </div>
+        <>
+            <Routes>
+                <Route
+                    path='/sso-callback'
+                    element={<AuthenticateWithRedirectCallback signUpForceRedirectUrl={"/auth-callback"} />}
+                />
+                <Route path='/auth-callback' element={<AuthCallbackPage />} />
+                <Route path='/admin' element={<AdminPage />} />
+
+                <Route element={<MainLayout />}>
+                    <Route path='/' element={<HomePage />} />
+                    <Route path='/chat' element={<ChatPage />} />
+                    <Route path='/albums/:albumId' element={<AlbumPage />} />
+                    <Route path='*' element={<NotFoundPage />} />
+                </Route>
+            </Routes>
+            <Toaster />
+        </>
     );
-};
+}
 
 export default App;
